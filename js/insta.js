0f831234c697d32f52797de293ed898a9e8973d2
bearbur <bearbur@gmail.com>
2017-02-20 15:51:23 +0300

    0.11

---
+++ js/insta.js
@@ -0,0 +1,8 @@
+
+
+var feed = new Instafeed({
+        accessToken: '4677492945.7a2bc5b.fa2f224ea0584db2a74a89526497f3ac',
+        clientId: '7a2bc5bf3e92458f840fd08c7a32f4c0',
+        userId: "truebearbur"
+    });
+    feed.run();
