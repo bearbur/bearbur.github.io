6ff05616573d651109a958f2ca78803d3c0c91a8
bearbur <bearbur@gmail.com>
2017-04-03 10:41:47 +0300

    psh

---
+++ js/photoScript.js
@@ -0,0 +1,11 @@
+$('.instagram-lite').instagramLite({
+	accessToken: '4677492945.7a2bc5b.fa2f224ea0584db2a74a89526497f3ac',
+  list: false,
+		urls: false,
+		success: function() {
+			console.log('The request was successful!');
+		},
+		error: function(errorCode, errorMessage) {
+			console.log('There was an error with the request');
+		}
+});
